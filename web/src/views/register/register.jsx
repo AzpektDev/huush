fd231799fad09ce6c4ca02e7a3c43ca55eae9606
SmoczaSkala <kamilo.dance2000@gmail.com>
2024-05-19 19:18:56 +0200

    Web Initial Commit

---
+++ web/src/views/register/register.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Register from "../../components/register/register";
+
+const Register = () => {
+    return (
+        <div>
+        <Register />
+        </div>
+    );
+}
