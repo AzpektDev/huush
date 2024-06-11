7bc29345bc231bdd1b8d36e09d294d4c14dab0c5
AzpektDev <65199167+AzpektDev@users.noreply.github.com>
2024-06-11 21:23:13 +0200

    fixed chats table

--- api/entities/Chat.ts
+++ api/entities/Chat.ts
@@ -1,40 +1,40 @@
 import { Request } from "express";
-import { BeforeInsert, BeforeUpdate, Column, Entity, JoinColumn, OneToMany, OneToOne, ManyToOne, ManyToMany, RelationId, Index } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, JoinTable, ManyToMany, ManyToOne, OneToMany, JoinColumn, Index } from "typeorm";
 import { Snowflake } from "../util/Snowflake";
-import { trimSpecial } from "../util/String";
 import { BaseClass } from "./BaseClass";
 import { Message } from "./Message";
 import { User } from "./User";
-import { get } from "http";
 
 @Entity("chats")
 export class Chat extends BaseClass {
   @Column()
-	@RelationId((chat: Chat) => chat.author)
-	@Index()
-	author_id?: string;
+  @Index()
+  author_id?: string;
 
-	@JoinColumn({ name: "author_id", referencedColumnName: "id" })
-	@ManyToOne(() => User, {
-		onDelete: "CASCADE",
+  @JoinColumn({ name: "author_id", referencedColumnName: "id" })
+  @ManyToOne(() => User, {
+    onDelete: "CASCADE",
   })
   author?: User;
     
-    
-  @Column({ nullable: true })
-  @RelationId((chat: Chat) => chat.participants)
-  @Index()
-  participants_ids?: string[];
-
-  @JoinColumn({ name: "participants_ids", referencedColumnName: "id" })
-  @ManyToMany(() => User, {
-    onDelete: "CASCADE",
+  @ManyToMany(() => User, user => user.chats, {
+    cascade: true
+  })
+  @JoinTable({
+    name: "chat_participants", // Name of the join table
+    joinColumn: {
+      name: "chat_id",
+      referencedColumnName: "id"
+    },
+    inverseJoinColumn: {
+      name: "user_id",
+      referencedColumnName: "id"
+    }
   })
   participants?: User[];
 
-
   @OneToMany(() => Message, message => message.chat)
-	messages?: Message[];
+  messages?: Message[];
 
   @Column()
   created_at: Date;
