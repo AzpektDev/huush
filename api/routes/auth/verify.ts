d16ccc36411b8b2fd8c231a4849094818080e0af
AzpektDev <65199167+AzpektDev@users.noreply.github.com>
2024-06-11 22:07:32 +0200

    POST /auth/verify - verify the otp

---
+++ api/routes/auth/verify.ts
@@ -0,0 +1,62 @@
+import bcrypt from "bcrypt";
+import { Request, Response, Router } from "express";
+import { User } from "../../entities/User";
+import { sendOTP } from "../../util/SMS";
+import { OTP } from "../../entities/OTP";
+
+const router: Router = Router();
+
+interface RegisterSchema {
+  code: string;
+}
+
+router.post("/", async (req: Request, res: Response) => {
+  let { code }: RegisterSchema = req.body;
+
+  // Validate the request body
+  if (!code) {
+    return res.status(400).send({
+      message: "Missing required fields",
+      code: 0,
+      status: 400,
+    });   
+  }
+
+  const otp = await OTP.findOneBy({
+    otp: code,
+  });
+
+  if (!otp) {
+    return res.status(400).send({
+      message: "Invalid code",
+      code: 0,
+      status: 400,
+    });
+  }
+
+  // delete the otp
+  await OTP.delete(otp.id);
+
+  const user = await User.findOneBy({
+    id: otp.user_id,
+  });
+
+  if (!user) {
+    return res.status(400).send({
+      message: "Invalid user",
+      code: 0,
+      status: 400,
+    });
+  }
+
+  user.phone_verified = true;
+  await user.save();
+
+  return res.status(200).send({
+    message: "Successfully verified",
+    code: 0,
+    status: 200,
+  });
+});
+
+export default router;
