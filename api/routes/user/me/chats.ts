4050f142757f4fba37e2f8b731979e5d60cb6f65
AzpektDev <65199167+AzpektDev@users.noreply.github.com>
2024-06-12 23:36:47 +0200

    add last message to /user/me/chats

--- api/routes/user/me/chats.ts
+++ api/routes/user/me/chats.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { Chat } from "../../../entities/Chat";
 import { Authorization } from "../../../middlewares/Authorization";
+import { Message } from "../../../entities/Message";
 const router: Router = Router();
 
 router.get("/", Authorization, async (req: Request, res: Response) => {
@@ -8,6 +9,20 @@ router.get("/", Authorization, async (req: Request, res: Response) => {
     .where("chat.participant_ids @> :participantId", { participantId: [req.user.id] })
     .getMany();
 
+  for (const chat of chats) {
+    const lastMessage = await Message.createQueryBuilder("message")
+      .where("message.chat_id = :chatId", { chatId: chat.id })
+      .orderBy("message.created_at", "DESC")
+      .getOne();
+
+    if (lastMessage) {
+      chat["last_message"] = lastMessage.content;
+    } else {
+      chat["last_message"] = "No messages yet...";
+    }
+  }
+  
+  
   res.json({
     message: "Successfully fetched chats",
     code: 0,
